73470796ed417e6fed68864171dc5da6f29d0548
msansal <msansal@mail.com>
2018-09-27 12:52:20 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # MM-Challenge-1st
 Develop an AWS S3 analysis tool.
+The tool is a shell command line utility that returns information over all S3 buckets in an Amazon account.
+Exemple of returns:
+```sh
+# -- BUCKET number  1  ----- #
+Bucket name:  2017.uxrumble.com
+Creation date:  2018-03-17
+Number of files:  24
+Total size of files:  4.0 MiB
+```
+
+1. Prerequisites
+- Install AWS CLI
+    [Install](https://docs.aws.amazon.com/es_es/cli/latest/userguide/installing.html) 
+- Configure AWS 
+  [Configure](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html)
+
+  
+
+2. Use
+   -
+   - Clone the repository
+  ```sh
+  git clone https://github.com/msansal/MM-Challenge-1st.git
+  ```
+  - Execute
+  ```sh
+  ./aws_s3.sh
+  ```
---
+++ aws_s3.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -x
+############################################
+#    AWS S3 Analysis tool
+############################################
+#
+#  title           :aws_s3.sh
+#  description     :AWS s3 analysis tool
+#  author		 :msantos
+#  date            :20180919
+#  version         :0.1
+#  usage		 :bash mkscript.sh
+#  notes           :Prerequissites: Install AWS Cli and configure your credentials.
+#  bash_version    :
+#
+#==============================================================================
+# Variables
+NAME='' #NAME of Bucket
+C_DATE='' # Date of creation
+NFILES='' # Number of files
+TSIZE='' # Total size of the bucket
+NUM=1
+# List the buckets
+aws s3 ls > liste.txt
+
+while IFS=' ' read -r line || [[ -n "$line" ]]; do
+    #read NAME
+    NAME=$(echo $line | cut -d ' ' -f3)
+    #read  CDATE
+    C_DATE=$(echo $line | cut -d ' ' -f1)
+    # Comand aws for totalsize and number of files
+    aws s3 ls s3://$NAME --recursive --human-readable --summarize > size.txt
+    tail -2 size.txt > size_tail.txt
+    #read Size
+    l1=`tail -1 size_tail.txt`
+    TSIZE=$(echo $l1 | cut -d ':' -f2)
+    #read Number files
+    l2=`head -1 size_tail.txt`
+    NFILES=$(echo $l2 | cut -d ':' -f2)
+
+  # RETURN INFORMATION
+  echo "# -- BUCKET number " $NUM " ----- #"
+  echo "Bucket name: " $NAME
+  echo "Creation date: " $C_DATE
+  echo "Number of files: " $NFILES
+  echo "Total size of files: " $TSIZE
+  echo "  "
+  # Number of buckets
+  NUM=$(($NUM +1))
+
+ done < liste.txt
